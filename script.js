b5c9d3d4bb6c2b91447c5d350bcdcd9f43abf7a7
sean stephen brian <seanstephenbrian@gmail.com>
2022-10-06 11:44:15 -0400

    Fix bug (playAgain firing 2x) by cleaning up code
    Added comments throughout to describe what is really happening;
    isolate code that only needs to be fired once (primarily dealing
    with event listeners) as anonymous 'IIFE' functions;
    wrap the entire Javascript in an anonymous immediately invoked function

--- script.js
+++ script.js
@@ -15,6 +15,7 @@
             c3: ''
         };
 
+        // render the gameboard using the current contents of the currentBoard object:
         function renderBoard() {
             for (square in currentBoard) {
                 let squareDiv = document.querySelector(`.${square}`);
@@ -27,11 +28,13 @@
             }
         }
 
+        // update the currentBoard object by changing the value of a specific square/symbol pairing, then re-render the board:
         function updateBoard(square, marker) {
             currentBoard[square] = marker;
             renderBoard();
         }
 
+        // empty the gameboard by making the value of each item in the object empty then re-rendering the board:
         function emptyBoard() {
             for (square in currentBoard) {
                 let squareDiv = document.querySelector(`.${square}`);
@@ -41,16 +44,19 @@
             renderBoard();
         }
 
-        function addClickListeners() {
+        // add listeners for clicks on the game squares; a click on any square will trigger the markSquare function
+        (function() {
             let squares = document.querySelectorAll('.square');
             squares.forEach(square => {
                 square.addEventListener('click', (e) => {
                     Gameplay.markSquare(e);
                 });
             });
-        }
+        })();
 
+        // check for winner by looking for three-in-a-row sequences of the same symbol on the gameboard; if there is one, return the winning symbol
         function checkForWinner() {
+
             if ((currentBoard.a1) && (currentBoard.a1 === currentBoard.a2) && (currentBoard.a2 === currentBoard.a3)) {
                 return currentBoard.a1;
             } else if ((currentBoard.b1) && (currentBoard.b1 === currentBoard.b2) && (currentBoard.b2 === currentBoard.b3)) {
@@ -68,6 +74,8 @@
             } else if ((currentBoard.a3) && (currentBoard.a3 === currentBoard.b2) && (currentBoard.b2 === currentBoard.c1)) {
                 return currentBoard.a3;
             } 
+
+            // if there isn't a winner, proceed to check if all nine squares are filled; if they are, declare a tie:
             let squares = document.querySelectorAll('.square');
             let i = 0;
             squares.forEach(square => {
@@ -84,14 +92,11 @@
             updateBoard,
             renderBoard,
             emptyBoard,
-            addClickListeners,
             checkForWinner
         };
-
     })();
 
-
-    // factory function to create new Player objects:
+    // function to create new Player objects:
     const Player = (name, marker) => {
         return { name, marker };
     };
@@ -99,41 +104,46 @@
     // Gameplay module:
     const Gameplay = (function () {
 
+        // initialize empty objects/variables that need to be accessed by the methods in this module:
         let playerOne = Player('','');
         let playerTwo = Player('','');
         let currentPlayer = '';
         let currentPlayerName = '';
         let round;
 
+        // start the game by emptying the board & setting the round number to 1
         function startGame(playerOneName, playerOneMarker, playerTwoName, playerTwoMarker) {
-
             Gameboard.emptyBoard();
 
             round = 1;
 
+            // use passed values from the player form to set the playerOne and playerTwo object properties:
             playerOne.name = playerOneName;
             playerOne.marker = playerOneMarker;
             
             playerTwo.name = playerTwoName;
             playerTwo.marker = playerTwoMarker;
 
+            // make playerOne the currentPlayer
             currentPlayer = playerOne.marker;
             currentPlayerName = playerOne.name;
 
+            // double-check that there is a currentPlayer, and if so change the background image to that player's symbol
             if (currentPlayer) {
                 document.body.classList.remove(`init-bg`);
                 document.body.classList.add(`${currentPlayer}-bg`);
             }
 
+            // set the header text to the current player's name
             if (currentPlayerName) {
                 const header = document.querySelector('header');
                 header.innerText = `${currentPlayerName}'s turn`;
             }
-
         };
 
         startGame();
 
+        // this function fires instead of startGame() when the user clicks the play again button:
         function playAgain() {
 
             let winningPlayer = currentPlayer;
@@ -144,6 +154,8 @@
 
             round = 1;
 
+            // check if the previous round's playerOne (who had the first turn in the previous round) was the winner;
+            // if they were, switch playerOne and playerTwo so that the previous round's losing player now gets the first turn in the new round:
             if (winningPlayer === playerOne.marker) {
 
                 let playerOneName = playerOne.name;
@@ -158,23 +170,31 @@
 
             }
 
+            // make sure currentPlayer is set to playerOne
             currentPlayer = playerOne.marker;
             currentPlayerName = playerOne.name;
 
+            // update the background image with the current player's symbol
             document.body.classList.remove(`x-bg`);
             document.body.classList.remove(`o-bg`);
             document.body.classList.add(`${currentPlayer}-bg`);
 
+            // update header with current player's name
             const header = document.querySelector('header');
             header.innerText = `${currentPlayerName}'s turn`;
 
         }
 
         function updateCurrentPlayer() {
+
+            // if the players haven't yet chosen symbols, set the background to a default initial background (an X)
             if (playerOne.marker === undefined || playerTwo.marker === undefined) {
                 document.body.classList.remove(`x-bg`);
                 document.body.classList.remove(`o-bg`);
                 document.body.classList.add(`init-bg`);
+            // if the game has started, increment the round,  
+            // change the currentPlayer (knowing that playerOne is the currentPlayer for odd rounds and playerTwo is the currentPlayer for even rounds),
+            // and update the background & header with the currentPlayer's symbol & name
             } else if (playerOne.marker || playerTwo.marker) {
                 document.body.classList.remove(`x-bg`);
                 document.body.classList.remove(`o-bg`);
@@ -197,8 +217,11 @@
         function markSquare(e) {
 
             const clickedSquare = e.target;
+            // if the square is already filled, exit the function
             checkSquare: if (clickedSquare.innerText) {
                 break checkSquare;
+            // if the square is empty, update the currentBoard object by setting the clicked square's value to the currentPlayer's symbol,
+            // then check for a winner; if there is a winner, display the winner message; if there isn't, proceed with gameplay by updating the current player.
             } else if (!clickedSquare.innerText) {
                 const squareId = clickedSquare.dataset.id;
                 Gameboard.updateBoard(squareId, currentPlayer);
@@ -217,23 +240,21 @@
             updateCurrentPlayer,
             playAgain
             }
-
     })();
-
 
     //  module for dynamic page effects:
     const PageEffects = (function() {
 
-        function addHeaderLink() {
+        // header takes you to tic-tac-toe page on wikihow when clicked:
+        (function() {
             const header = document.querySelector('header');
             header.addEventListener('click', () => {
                 window.open('https://www.wikihow.com/Play-Tic-Tac-Toe');
             });
-        }
+        })();
 
-        addHeaderLink();
-
-        function addSquareListeners() {
+        // add mouseover/mouseout listeners for grid squares to apply rotation effects:
+        (function() {
             const squares = document.querySelectorAll('.square');
             squares.forEach(square => {
                 square.addEventListener('mouseover', (e) => {
@@ -243,8 +264,9 @@
                     PageEffects.rotateSquare(e);
                 });
             });
-        }
+        })();
 
+        // if it's not a mobile viewport, rotate the square a random number of degrees when hovered over:
         function rotateSquare(e) {
             if (window.innerWidth >= 751) {
                 const square = e.target;
@@ -253,14 +275,13 @@
             }
         }
 
-        function addClickableListener() {
-            const clickable = document.querySelector('.clickable');
-            clickable.addEventListener('click', () => {
+        // the new player form is revealed when the user clicks the 'click to start a game' button:
+        (function() {
+            const clickToStart = document.querySelector('.click-to-start-text');
+            clickToStart.addEventListener('click', () => {
                 showPlayerForm();
             });
-        }
-
-        addClickableListener();
+        })();
 
         function showPlayerForm() {
             const playerForm = document.querySelector('.form-window');
@@ -281,10 +302,6 @@
             alert.classList.remove('hide');
             const alertOverlay = document.querySelector('.alert-overlay');
             alertOverlay.classList.remove('hide');
-            const closeAlert = document.querySelector('.alert-close-button');
-            closeAlert.addEventListener('click', () => {
-                hideAlert();
-            }, {once : true});
         }
 
         function hideAlert() {
@@ -295,15 +312,6 @@
         }
 
         function showTieAlert() {
-            const tieCloseButton = document.querySelector('.tie-close-button');
-            tieCloseButton.addEventListener('click', () => {
-                window.location.href = 'https://www.wikihow.com/Win-at-Everything';
-            });
-            const tiePlayAgainButton = document.querySelector('.tie-play-again-button');
-            tiePlayAgainButton.addEventListener('click', () => {
-                hideTieAlert();
-                Gameplay.playAgain();
-            });
             const tieAlert = document.querySelector('.tie');
             tieAlert.classList.remove('hide');
             const winnerOverlay = document.querySelector('.winner-overlay');
@@ -318,14 +326,6 @@
         }
 
         function showWinner(winnerName) {
-            const winnerCloseButton = document.querySelector('.winner-close-button');
-            winnerCloseButton.addEventListener('click', () => {
-                window.location.href = 'https://www.wikihow.com/Play-Human-Tic-Tac-Toe';
-            });
-            const winnerPlayAgainButton = document.querySelector('.winner-play-again-button');
-            winnerPlayAgainButton.addEventListener('click', () => {
-                Gameplay.playAgain();
-            });
             const winnerWindow = document.querySelector('.winner');
             winnerWindow.classList.remove('hide');
             const winnerOverlay = document.querySelector('.winner-overlay');
@@ -346,8 +346,11 @@
             startDiv.classList.add('hide');
         }
 
-        function addFormListeners() {
+        // this function adds all the events listeners to the player form:
+        (function() {
             const playerForm = document.querySelector('#player-form');
+            // when the user submits a valid form, set the marker symbols for both players,
+            // checking for possible variation in capitalization and the use of '0' for 'o'
             playerForm.addEventListener('submit', (e) => {
                 e.preventDefault();
                 let playerOneMarkerInput = document.querySelector('#player-one-marker').value;
@@ -365,20 +368,25 @@
                     playerTwoMarker = 'o';
                 }
                 const computerOption = document.querySelector('#computer');
+
+                // if the players have selected the same symbol, display an alert
                 if (!computerOption.checked && (playerOneMarker === playerTwoMarker)) {
                     showAlert();
                     const alertText = document.querySelector('.alert-text');
                     alertText.innerText = 'players must choose different symbols!';
                 }
+
+                // if the players have selected different symbols, or if player one has selected a symbol and checked the computer opponent option, start the game:
                 if ((playerOneMarker !== playerTwoMarker) || (playerOneMarker && computerOption.checked)) {
                     clickedPlay(e);
                 }
             });
             const playerMarkerInputs = document.querySelectorAll('.player-marker-input');
+
+            // adapt the submitted form validation code to dynamically validate the form as the user focuses into and focuses out of specific form elements:
             playerMarkerInputs.forEach(input => {
                 input.addEventListener('focusout', () => {
 
-                    // same code originally written for form submit event listener; it checks if both symbol inputs are the same; if they are, it shows an alert asking the players to pick different symbols
                     let playerOneMarkerInput = document.querySelector('#player-one-marker').value;
                     let playerOneMarker;
                     if (playerOneMarkerInput === 'X' || playerOneMarkerInput === 'x') {
@@ -411,6 +419,8 @@
 
                 })
             });
+
+            // make the player two section disappear from the form if the user checks the computer opponent option:
             const radioInputs = document.querySelectorAll(`input[type='radio']`);
             radioInputs.forEach(option => {
                 option.addEventListener('click', () => {
@@ -431,17 +441,45 @@
             closeButton.addEventListener('click', () => {
                 hidePlayerForm();
             });
-        }
+        })();
 
-        addFormListeners();
+        // this function adds event listeners for the clickable elements on various alert/pop-up windows:
+        (function() {
 
+            // click listeners for the close button and play again buttons on the winner alert pop-up window:
+            const winnerCloseButton = document.querySelector('.winner-close-button');
+            winnerCloseButton.addEventListener('click', () => {
+                window.location.href = 'https://www.wikihow.com/Play-Human-Tic-Tac-Toe';
+            });
+            const winnerPlayAgainButton = document.querySelector('.winner-play-again-button');
+            winnerPlayAgainButton.addEventListener('click', () => {
+                Gameplay.playAgain();
+            });
+
+            // click listeners for the close button on a standard alert pop-up:
+            const closeAlert = document.querySelector('.alert-close-button');
+            closeAlert.addEventListener('click', () => {
+                hideAlert();
+            });
+
+            // click listeners for the close button and play again buttons on the tie alert pop-up window:
+            const tieCloseButton = document.querySelector('.tie-close-button');
+            tieCloseButton.addEventListener('click', () => {
+                window.location.href = 'https://www.wikihow.com/Win-at-Everything';
+            });
+            const tiePlayAgainButton = document.querySelector('.tie-play-again-button');
+            tiePlayAgainButton.addEventListener('click', () => {
+                hideTieAlert();
+                Gameplay.playAgain();
+            });
+        })();
+
+        // when the user clicks the play button, fire the startGame function using the relevant values from the submitted form:
         function clickedPlay(e) {
 
-            Gameboard.addClickListeners();
-            PageEffects.addSquareListeners();
             PageEffects.hidePlayerForm();
             PageEffects.hideStartDiv();
-
+            
             const playerOne = document.querySelector('#player-one').value;
             const playerOneMarkerInput = document.querySelector('#player-one-marker').value;
             let playerOneMarker;
@@ -450,6 +488,8 @@
             } else if (playerOneMarkerInput === 'O' || playerOneMarkerInput === 'o' || playerOneMarkerInput === '0') {
                 playerOneMarker = 'o';
             }
+
+            // if the computer option is checked, automatically set the computer (playerTwo) symbol to the one that the user did not select:
             const computerOption = document.querySelector('#computer');
             if (computerOption.checked) {
                 const playerTwo = 'computer';
@@ -471,49 +511,41 @@
                 }
                 Gameplay.startGame(playerOne, playerOneMarker, playerTwo, playerTwoMarker);
             }
-
         }
-
-        function setBodyHeight() {
-            const windowHeight = window.innerHeight + "px";
-            document.body.style.minHeight = windowHeight;
-            document.body.style.maxHeight = windowHeight;
-        }
-
-        setBodyHeight();
         
-        function addWindowResizeListener() {
-            addEventListener('resize', () => {
+        // check height of screen; if it's a mobile screen (height < 751px), 
+        // check for the window height and set body min-height & max-height to that exact value;
+        // otherwise set the min-height to 100vh:
+        (function() {
+            function setBodyHeight() {
                 if (window.innerHeight < 751) {
-                    setBodyHeight();
+                    const windowHeight = window.innerHeight + "px";
+                    document.body.style.minHeight = windowHeight;
+                    document.body.style.maxHeight = windowHeight;
                 } else if (window.innerHeight >= 751) {
                     document.body.style.minHeight = '100vh';
                     document.body.style.maxHeight = null;
                 }
-            });
-        }
+            }
 
-        addWindowResizeListener();
+            setBodyHeight();
+            
+            addEventListener('resize', () => {
+                setBodyHeight();
+            });
+        })();
         
         return {
-            addHeaderLink,
-            addSquareListeners,
             rotateSquare,
             showPlayerForm,
             hidePlayerForm,
-            addClickableListener,
             hideStartDiv,
-            addFormListeners,
-            setBodyHeight,
-            addWindowResizeListener,
             showAlert,
             hideAlert,
             showWinner,
             hideWinner,
             showTieAlert,
-            hideTieAlert
+            hideTieAlert,
         }
-        
     })();
-
 })();
